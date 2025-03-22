8b1c3870d06800c954b1b777cbad48182f164e7e
daikaduc05 <danghoaiduc9@gmail.com>
2025-03-22 19:44:05 +0700

    inint backend project

---
+++ BE/TaskManagerBE/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerBE.Models;
+
+namespace TaskManagerBE.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    }
+}
